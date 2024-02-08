3f3390403b481807d0db68c215918ce8c19d183e
mardeva <146278852+mardeva@users.noreply.github.com>
2024-02-08 00:46:29 -0300

    agregadas lineas separadoras entre articles, botones y texto funcionando. Falta ciclo y eliminar botón '-' al inicio

--- index.js
+++ index.js
@@ -1,56 +1,67 @@
 const btnOpen = document.getElementById('article__button--open');
 const btnClose = document.getElementById('article__button--close');
-console.log(btnOpen);
-console.log(btnClose);
-const txt = document.getElementById('description')
-console.log(txt)
+
+const txt = document.getElementById('description');
+
+
 
 // abrir boton +
 btnOpen.addEventListener('click', (e) => {
     const btn = e.target.closest('button');
-    
     console.log(btn);
 
     if (btn?.dataset?.action === 'open-text') {
         // eliminar vista boton +
         btn.classList.add('article__button--open__disabled');
-
         // mostrar parrafo
-        txt.classList.remove('article__text--disabled')
+        txt.classList.remove('article__text--disabled');
         console.log('borrado');
+
     }
 });
 
+//CERRAR
 btnClose.addEventListener('click', (e) => {
+    
+
     const btn = e.target.closest('button');
-    console.log(btn);
+
+    console.log('boton cerrar: ' + btn);
 
     if (btn?.dataset?.action === 'close-text') {
-        
-        txt.classList.add('article__button--open__disabled')
-        btn.classList.add('article__button--close--disabled');
-        
 
+        btn.classList.add('article__button--open__disabled');
+        txt.classList.add('article__button--open__disabled');        
+        btnOpen.classList.remove('article__button--open__disabled')
+        
+        
+        console.log('cerrado');
+        
     }
-    return;
-    // }else if(btn?.dataset?.action === 'close-text'){
 
-    // } 
+
+
 });
 
 
 
 
-// button.addEventListener('click', ()={
-//     console.log('click')
-// })
-// const collapse = () => {
-//     if (true) {
-//         const openAnswer = button.className.add('article__button--active')
-//     }else{
-//         const closeAnswer = button.className.remove('article__button--active')
-//     }
-//     return
-// };
 
-// export default collapse()
+
+
+// // button.addEventListener('click', ()={
+// //     console.log('click')
+// // })
+// // const collapse = () => {
+// //     if (true) {
+// //         const openAnswer = button.className.add('article__button--active')
+// //     }else{
+// //         const closeAnswer = button.className.remove('article__button--active')
+// //     }
+// //     return
+// // };
+
+// // export default collapse()
+
+
+// otro
