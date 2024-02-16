eeeb18971560716de20dbcf1abc148e2402ebb54
mardeva <146278852+mardeva@users.noreply.github.com>
2024-02-16 14:47:11 -0300

    disenos mobile y web finalizados en archivos de prueba - falta funcionalidad

--- prueba.js
+++ prueba.js
@@ -1,71 +1,68 @@
-const btn = document.getElementById('article__button--open');
-const btn2 = document.getElementById('article__button--close');
+const btnOpen = document.getElementById('article__button--open');
+const btnClose = document.getElementById('article__button--close');
 const txt = document.getElementById('description');
 
+const contenedorArticulos = document.getElementById('section__container--articles');
 
-// abrir boton +
-btn.addEventListener('click', (e) => {
-    const btnOpen = e.target.closest('button');
+contenedorArticulos.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e.target.closest('button'))
+    const boton = e.target.closest('button')
+    // if (e.target.closest('button')) {
 
-    if (btnOpen?.dataset?.action === "open-text") {
-        btn.classList.add('article__button--open__disabled');
-        txt.classList.remove('article__text--disabled');
-        btn2.classList.remove('article__button--close__disabled');
-    }
-
-
-    btn2.addEventListener('click', (e) => {
-        const btnClose = e.target.closest('button');
-
-        if (btn2?.dataset?.action === 'close-text') {
-            btn.classList.remove('article__button--open__disabled');
-            txt.classList.add('article__text--disabled');
-            btn2.classList.add('article__button--close__disabled');
-        }
-
-    });
-
-
-
-
-
-    // if (btn?.dataset?.action === 'open-text') {
-    //     // eliminar vista boton +
-    //     btn.classList.add('article__button--open__disabled');
-    //     // mostrar parrafo
+    //     //quito vista boton +
+    //     btnOpen.classList.add('article__button--open__disabled');
+    //     //muestro texto respuesta
     //     txt.classList.remove('article__text--disabled');
-    //     console.log('borrado');
+    //     //agrego vista boton -
+    //     btnClose.classList.remove('article__button--close__disabled');
 
-    // } else if (btn?.dataset?.action === 'close-text') {
-    //     btn.classList.add('article__button--open__disabled');
-    //     txt.classList.add('article__button--open__disabled');
-    //     btnOpen.classList.remove('article__button--open__disabled');
+    //     console.log(typeof contenedorArticulos)
+    //     console.log(e.target.dataset.action)
 
-    //     console.log('cerrado');
-
+    //     const tipoBoton = e.target.dataset.action
+    //     console.log(tipoBoton)
+        
     // }
+    
 });
 
 
 
 
 
+// // Funciona solo en el primer boton
+// // abrir boton + y abrir texto
+// btn.addEventListener('click', (e) => {
+//     const btnOpen = e.target.closest('button');
+
+//     console.log(typeof btnOpen);
+
+//     const abrir = () => {
+//         if (btnOpen?.dataset?.action === "open-text") {
+//             btn.classList.add('article__button--open__disabled');
+//             txt.classList.remove('article__text--disabled');
+//             btn2.classList.remove('article__button--close__disabled');
+//         }
+//     };
+
+//     abrir();
+//     console.log('la funcion abrir funciona!');
+
+//     // cerrar botón "-" y texto
+//     const cerrar = () => {
+//         btn2.addEventListener('click', (e) => {
+//             const btnClose = e.target.closest('button');
+
+//             if (btnClose?.dataset?.action === 'close-text') {
+//                 btn.classList.remove('article__button--open__disabled');
+//                 txt.classList.add('article__text--disabled');
+//                 btn2.classList.add('article__button--close__disabled');
+//             }
+//         });
+//     };
+//     cerrar();
 
 
+// });
 
-// // button.addEventListener('click', ()={
-// //     console.log('click')
-// // })
-// // const collapse = () => {
-// //     if (true) {
-// //         const openAnswer = button.className.add('article__button--active')
-// //     }else{
-// //         const closeAnswer = button.className.remove('article__button--active')
-// //     }
-// //     return
-// // };
-
-// // export default collapse()
-
-
-// otro
