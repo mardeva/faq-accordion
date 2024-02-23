80f1943f76ba5153cc516c6adce18db277341544
mardeva <146278852+mardeva@users.noreply.github.com>
2024-02-23 16:41:04 -0300

    funcionalidad botones arreglada

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const btn = document.getElementById('article__button--open');
 const btn2 = document.getElementById('article__button--close');
+
+
 const txt = document.getElementById('description');
 
 // Funciona solo en el primer boton
 // abrir boton + y abrir texto
 btn.addEventListener('click', (e) => {
     const btnOpen = e.target.closest('button');
+    
+    console.log( typeof btnOpen)
 
-    if (btnOpen?.dataset?.action === "open-text") {
-        btn.classList.add('article__button--open__disabled');
-        txt.classList.remove('article__text--disabled');
-        btn2.classList.remove('article__button--close__disabled');
-    }
+    const abrir = () => {
+        if (btnOpen?.dataset?.action === "open-text") {
+            btn.classList.add('article__button--open__disabled');
+            txt.classList.remove('article__text--disabled');
+            btn2.classList.remove('article__button--close__disabled');
+        }
+    };
+
+    abrir();
+    console.log('la funcion abrir funciona!');
 
     // cerrar botón "-" y texto
-    btn2.addEventListener('click', (e) => {
-        const btnClose = e.target.closest('button');
+    const cerrar = () => {
+        btn2.addEventListener('click', (e) => {
+            const btnClose = e.target.closest('button');
 
-        if (btnClose?.dataset?.action === 'close-text') {
-            btn.classList.remove('article__button--open__disabled');
-            txt.classList.add('article__text--disabled');
-            btn2.classList.add('article__button--close__disabled');
-        }
-
-    });
+            if (btnClose?.dataset?.action === 'close-text') {
+                btn.classList.remove('article__button--open__disabled');
+                txt.classList.add('article__text--disabled');
+                btn2.classList.add('article__button--close__disabled');
+            }
+        });
+    };
+    cerrar();
 
 
 });
+
 
 
 
--- prueba.js
+++ prueba.js
@@ -1,68 +1,26 @@
-const btnOpen = document.getElementById('article__button--open');
-const btnClose = document.getElementById('article__button--close');
-const txt = document.getElementById('description');
-
-const contenedorArticulos = document.getElementById('section__container--articles');
-
-contenedorArticulos.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log(e.target.closest('button'))
-    const boton = e.target.closest('button')
-    // if (e.target.closest('button')) {
-
-    //     //quito vista boton +
-    //     btnOpen.classList.add('article__button--open__disabled');
-    //     //muestro texto respuesta
-    //     txt.classList.remove('article__text--disabled');
-    //     //agrego vista boton -
-    //     btnClose.classList.remove('article__button--close__disabled');
-
-    //     console.log(typeof contenedorArticulos)
-    //     console.log(e.target.dataset.action)
-
-    //     const tipoBoton = e.target.dataset.action
-    //     console.log(tipoBoton)
-        
-    // }
-    
-});
+const btnOpen = document.querySelectorAll('.article__button--open');
+const btnClose = document.querySelectorAll('.article__button--close__disabled');
+const answer = document.querySelectorAll('.article__text');
 
 
+for (let i = 0; i < btnOpen.length; i++) {
+    console.log(btnOpen[i]);
+    btnOpen[i].addEventListener('click', function () {
+        console.log('boton abrir');
+        btnOpen[i].classList.toggle('article__button--open__disabled');
+        btnClose[i].classList.toggle('article__button--close__disabled');
+        answer[i].classList.toggle('article__text--disabled');
+    });
+
+}
 
 
+for (let i = 0; i < btnClose.length; i++) {
 
-// // Funciona solo en el primer boton
-// // abrir boton + y abrir texto
-// btn.addEventListener('click', (e) => {
-//     const btnOpen = e.target.closest('button');
+    btnClose[i].addEventListener('click', function () {
+        btnOpen[i].classList.toggle('article__button--open__disabled');
+        btnClose[i].classList.toggle('article__button--close__disabled');
+        answer[i].classList.toggle('article__text--disabled');
+    });
 
-//     console.log(typeof btnOpen);
-
-//     const abrir = () => {
-//         if (btnOpen?.dataset?.action === "open-text") {
-//             btn.classList.add('article__button--open__disabled');
-//             txt.classList.remove('article__text--disabled');
-//             btn2.classList.remove('article__button--close__disabled');
-//         }
-//     };
-
-//     abrir();
-//     console.log('la funcion abrir funciona!');
-
-//     // cerrar botón "-" y texto
-//     const cerrar = () => {
-//         btn2.addEventListener('click', (e) => {
-//             const btnClose = e.target.closest('button');
-
-//             if (btnClose?.dataset?.action === 'close-text') {
-//                 btn.classList.remove('article__button--open__disabled');
-//                 txt.classList.add('article__text--disabled');
-//                 btn2.classList.add('article__button--close__disabled');
-//             }
-//         });
-//     };
-//     cerrar();
-
-
-// });
-
+}
